be99d72ab6ccf0ca50d968460dad16fefdd27bc2
sergey104 <fil@localhost.localdomain>
2015-12-13 20:07:47 +0300

    totally new site

--- js/main.js
+++ js/main.js
@@ -1,189 +1,108 @@
-jQuery(function($) {'use strict';
+(function($){
+    $.fn.scrollingTo = function( opts ) {
+        var defaults = {
+            animationTime : 1000,
+            easing : '',
+            callbackBeforeTransition : function(){},
+            callbackAfterTransition : function(){}
+        };
 
-	// Navigation Scroll
-		(function($){
-		    $.fn.scrollingTo = function( opts ) {
-		        var defaults = {
-		            animationTime : 1000,
-		            easing : '',
-		            callbackBeforeTransition : function(){},
-		            callbackAfterTransition : function(){}
-		        };
+        var config = $.extend( {}, defaults, opts );
 
-		        var config = $.extend( {}, defaults, opts );
+        $(this).click(function(e){
+            var eventVal = e;
+            e.preventDefault();
 
-		        $(this).click(function(e){
-		            var eventVal = e;
-		            e.preventDefault();
+            var $section = $(document).find( $(this).data('section') );
+            if ( $section.length < 1 ) {
+                return false;
+            };
 
-		            var $section = $(document).find( $(this).data('section') );
-		            if ( $section.length < 1 ) {
-		                return false;
-		            };
+            if ( $('html, body').is(':animated') ) {
+                $('html, body').stop( true, true );
+            };
 
-		            if ( $('html, body').is(':animated') ) {
-		                $('html, body').stop( true, true );
-		            };
+            var scrollPos = $section.offset().top;
 
-		            var scrollPos = $section.offset().top;
+            if ( $(window).scrollTop() == scrollPos ) {
+                return false;
+            };
 
-		            if ( $(window).scrollTop() == scrollPos ) {
-		                return false;
-		            };
+            config.callbackBeforeTransition(eventVal, $section);
 
-		            config.callbackBeforeTransition(eventVal, $section);
-
-		            $('html, body').animate({
-		                'scrollTop' : (scrollPos+'px' )
-		            }, config.animationTime, config.easing, function(){
-		                config.callbackAfterTransition(eventVal, $section);
-		            });
-		        });
-		    };
-		}(jQuery));
-
-
-	$('.main-menu ul li a,.smooth-scroll').scrollingTo();
+            $('html, body').animate({
+                'scrollTop' : (scrollPos+'px' )
+            }, config.animationTime, config.easing, function(){
+                config.callbackAfterTransition(eventVal, $section);
+            });
+        });
+    };
+}(jQuery));
 
 
 
-	//Slider
-	$(document).ready(function() {
-		var time = 7; // time in seconds
-
-	 	var $progressBar,
-	      $bar, 
-	      $elem, 
-	      isPause, 
-	      tick,
-	      percentTime;
-	 
-	    //Init the carousel
-	    $("#main-slider").find('.owl-carousel').owlCarousel({
-	      slideSpeed : 500,
-	      paginationSpeed : 500,
-	      singleItem : true,
-	      navigation : true,
-			navigationText: [
-			"<i class='fa fa-angle-left'></i>",
-			"<i class='fa fa-angle-right'></i>"
-			],
-	      afterInit : progressBar,
-	      afterMove : moved,
-	      startDragging : pauseOnDragging,
-	      //autoHeight : true,
-	      transitionStyle : "fadeUp"
-	    });
-	 
-	    //Init progressBar where elem is $("#owl-demo")
-	    function progressBar(elem){
-	      $elem = elem;
-	      //build progress bar elements
-	      buildProgressBar();
-	      //start counting
-	      start();
-	    }
-	 
-	    //create div#progressBar and div#bar then append to $(".owl-carousel")
-	    function buildProgressBar(){
-	      $progressBar = $("<div>",{
-	        id:"progressBar"
-	      });
-	      $bar = $("<div>",{
-	        id:"bar"
-	      });
-	      $progressBar.append($bar).appendTo($elem);
-	    }
-	 
-	    function start() {
-	      //reset timer
-	      percentTime = 0;
-	      isPause = false;
-	      //run interval every 0.01 second
-	      tick = setInterval(interval, 10);
-	    };
-	 
-	    function interval() {
-	      if(isPause === false){
-	        percentTime += 1 / time;
-	        $bar.css({
-	           width: percentTime+"%"
-	         });
-	        //if percentTime is equal or greater than 100
-	        if(percentTime >= 100){
-	          //slide to next item 
-	          $elem.trigger('owl.next')
-	        }
-	      }
-	    }
-	 
-	    //pause while dragging 
-	    function pauseOnDragging(){
-	      isPause = true;
-	    }
-	 
-	    //moved callback
-	    function moved(){
-	      //clear interval
-	      clearTimeout(tick);
-	      //start again
-	      start();
-	    }
-	});
-
-	//Initiat WOW JS
+jQuery(document).ready(function(){
+	"use strict";
 	new WOW().init();
-	//smoothScroll
-	smoothScroll.init();
-
-	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
 
 
-
-	// Contact form
-	//var form = $('#main-contact-form');
-	//form.submit(function(event){
-		//scrollTo("footer");
-	//	
-		//document.getElementById("main-contact-form").reset(); 
-               // location.reload();
-		//event.preventDefault();
-		//Gentle_Anchors.Setup('#main-contact-form');
-	//	var form_status = $('<div class="form_status"></div>');
-	//	$.ajax({
-		//	url: $(this).attr('action'),
-		//	beforeSend: function(){
-		//		form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-		//	}
-		//}).done(function(data){
-		//	form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		//});
-	//});
-
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-
-function scrollTo(hash) {
-    location.hash = "#" + hash;
-    
-}
+(function(){
+ jQuery('.smooth-scroll').scrollingTo();
+}());
 
 });
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar-brand a").css("color","#fff");
+            $("#top-bar").removeClass("animated-header");
+        } else {
+            $(".navbar-brand a").css("color","inherit");
+            $("#top-bar").addClass("animated-header");
+        }
+    });
+
+    $("#clients-logo").owlCarousel({
+ 
+        itemsCustom : false,
+        pagination : false,
+        items : 5,
+        autoplay: true,
+
+    })
+
+});
+
+
+
+// fancybox
+$(".fancybox").fancybox({
+    padding: 0,
+
+    openEffect : 'elastic',
+    openSpeed  : 450,
+
+    closeEffect : 'elastic',
+    closeSpeed  : 350,
+
+    closeClick : true,
+    helpers : {
+        title : { 
+            type: 'inside' 
+        },
+        overlay : {
+            css : {
+                'background' : 'rgba(0,0,0,0.8)'
+            }
+        }
+    }
+});
+
