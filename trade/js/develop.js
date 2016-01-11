5ce1fc553133b12c8258f68ff32a1146213078db
Dmitry Zolotarev <dmitry.zolotarev@gmail.com>
2016-01-11 18:53:44 +0300

    market depth table javascript code

--- trade/js/develop.js
+++ trade/js/develop.js
@@ -2,208 +2,320 @@
  * Strem data handle
  */
 
-var pager = null;
-var entries = Array();
+var app = null;
 
-function DataEntry(symbol, bid2, bid1, bidSize, bidPrice, askPrice, askSize, ask1, ask2) {
-	this.symbol = symbol;
-	this.bid2 = bid2;
-	this.bid1 = bid1;
-	this.bidSize = bidSize;
-	this.bidPrice = bidPrice;
-	this.ask2 = ask2;
-	this.ask1 = ask1;
-	this.askSize = askSize;
-	this.askPrice = askPrice;
-	this.ss = "";
-	
+function Quote(/*Decimal*/price, /*Integer*/size) {
+	this.price = price;
+	this.size = size;
 }
 
-DataEntry.prototype.getSymbol = function() {
+Quote.prototype.getPrice = function() {
+	return this.price;
+}
+
+Quote.prototype.getSize = function() {
+	return this.size;
+}
+
+
+function MDEntry(/*String*/symbol) {
+	this.symbol = symbol;
+	this.asks = [];
+	this.bids = [];
+}
+
+MDEntry.prototype.getSymbol = function() {
 	return this.symbol;
 }
 
-DataEntry.prototype.getBid1 = function() {
-	return this.bid1;
+MDEntry.prototype.getBid = function(/*Integer*/index) {
+	return index >= 0 && index < this.bids.length ? this.bids[index] : null;
 }
 
-DataEntry.prototype.getBid2 = function() {
-	return this.bid2;
+MDEntry.prototype.getAsk = function(/*Integer*/index) {
+	return index >= 0 && index < this.asks.length ? this.asks[index] : null;
 }
 
-DataEntry.prototype.getBidSize = function() {
-	return this.bidSize;
+MDEntry.prototype.getBidPrice = function(/*Integer*/index) {
+	var quote = this.getBid(index);
+	return quote == null ? null : quote.getPrice();
 }
 
-DataEntry.prototype.getBidPrice = function() {
-	return this.bidPrice;
+MDEntry.prototype.getBidSize = function(/*Integer*/index) {
+	var quote = this.getBid(index);
+	return quote == null ? null : quote.getSize();
 }
 
-DataEntry.prototype.getAsk1 = function() {
-	return this.ask1;
+MDEntry.prototype.getAskPrice = function(/*Integer*/index) {
+	var quote = this.getAsk(index);
+	return quote == null ? null : quote.getPrice();
 }
-DataEntry.prototype.getAsk2 = function() {
-	return this.ask2;
+
+MDEntry.prototype.getAskSize = function(/*Integer*/index) {
+	var quote = this.getAsk(index);
+	return quote == null ? null : quote.getSize();
 }
-DataEntry.prototype.getAskSize = function() {
-	return this.askSize;
+
+MDEntry.prototype.addAsk = function(/*Quote*/quote) {
+	this.asks.push(quote);
 }
-DataEntry.prototype.getAskPrice = function() {
-	return this.askPrice;
+
+MDEntry.prototype.addBid = function(/*Quote*/quote) {
+	this.bids.push(quote);
 }
 
 
-function TableObject() {
-  
-  this.table_page = 0;
-  this.entries = new Array();
-  
-  this.timestamp = '';
-  
-  
-  pager = this;	
+function MDTableModel() {
+	this.entries = [];
+	this.symbol2index = {};
+	this.timestamp = null;
 }
-TableObject.prototype.updateEntryBySymbol = function(entry) {
-	var index = this.getEntryIndexBySymbol(entry.symbol);
-	var length = this.entries.length;
-	if ( index < 0 ) {
-		entries[length+1] = entry;
+
+MDTableModel.prototype.getTimestamp = function() {
+	return this.timestamp;
+}
+
+MDTableModel.prototype.setTimestamp = function(/*String*/timestamp) {
+	this.timestamp = timestamp;
+}
+
+MDTableModel.prototype.updateEntry = function(/*MDEntry*/entry) {
+	var symbol = entry.getSymbol();
+	if (symbol in this.symbol2index) {
+		var index = this.symbol2index[symbol];
+		this.entries[index] = entry;
+	} else {
+		var index = this.entries.length;
+		this.entries.push(entry); 
+		this.symbol2index[symbol] = index;
 	}
-	else {
-		entries[index] = entry;
-	}
-	this.refresh();
-}
-TableObject.prototype.getEntryIndexBySymbol = function(/*String*/ symbol) {
-	var length = this.entries.length;
-	for ( var i = 0; i < length; i ++ ) {
-		var entry = this.entries[i];
-		if ( entry.symbol == symbol ) {
-			return i;
-		}
-	}
-	return -1;
 }
 
-TableObject.prototype.getEntryBySymbol = function(/*String*/symbol) {
-	var index = this.getEntryIndexBySymbol(symbol);
-	if ( index < 0 ) {
-		throw "No symbol entry found: " + symbol;
+MDTableModel.prototype.getEntryCount = function() {
+	return this.entries.length;
+}
+
+MDTableModel.prototype.getEntryByIndex = function(/*Index*/index) {
+	if ( index < 0 || index >= this.getEntryCount() ) {
+		throw new Error(index, "Index out of range");
 	}
 	return this.entries[index];
 }
 
 
-
-TableObject.prototype.refresh = function() {
-
-var table = document.getElementById("market");
-document.getElementById("timestamp").innerHTML= '';
- 
-for( i = 0; i < 10; i++) {
-     
-     for( j = 0; j < 9; j++) {
-     d = this.table_page*10 +i;
-     
-     	if(this.entries == "undefined")
-          {table.rows[i+1].cells.item(j).innerHTML = this.entries[d][j];}
-      else 
-      	{table.rows[i+1].cells.item(j).innerHTML = ' ';}
-        
-    
-
-
-  }
-
-
-}
+function MDTableRow(/*Integer*/index) {
+	this.entry = null;
+	this.index = index;
+	this.tr = document.createElement("tr");
+	this.td_symbol = this.addCell("left");
+	this.td_bidSize2 = this.addCell();
+	this.td_bidSize1 = this.addCell();
+	this.td_bidSize0 = this.addCell();
+	this.td_bidPrice0 = this.addCell();
+	this.td_askPrice0 = this.addCell();
+	this.td_askSize0 = this.addCell();
+	this.td_askSize1 = this.addCell();
+	this.td_askSize2 = this.addCell();
 }
 
+MDTableRow.prototype.addCell = function(/*String*/cssClassName) {
+	var cell = document.createElement("td");
+	if ( typeof cssClassName !== "undefined" ) {
+		cell.className = cssClassName;
+	}
+	this.tr.appendChild(cell);
+	return cell;
+}
+
+MDTableRow.prototype.getRowElement = function() {
+	return this.tr;
+}
+
+MDTableRow.prototype.getIndex = function() {
+	return this.index;
+}
+
+MDTableRow.prototype.getEntry = function() {
+	return this.entry;
+}
+
+MDTableRow.prototype.update = function(/*MDEntry*/entry) {
+	if ( typeof(entry) == "undefined" ) {
+		throw new Error(this.index, "Illegal argument exception");
+	}
+	this.entry = entry;
+	this.setCellText(this.td_symbol, entry.getSymbol());
+	this.setCellText(this.td_bidSize2, entry.getBidSize(2));
+	this.setCellText(this.td_bidSize1, entry.getBidSize(1));
+	this.setCellText(this.td_bidSize0, entry.getBidSize(0));
+	this.setCellText(this.td_bidPrice0, entry.getBidPrice(0));
+	this.setCellText(this.td_askPrice0, entry.getAskPrice(0));
+	this.setCellText(this.td_askSize0, entry.getAskSize(0));
+	this.setCellText(this.td_askSize1, entry.getAskSize(1));
+	this.setCellText(this.td_askSize2, entry.getAskSize(2));
+}
+
+MDTableRow.prototype.setCellText = function(/*DOMElement*/td_cell, /*String*/text) {
+	td_cell.innerHTML = (text == null ? "" : text);
+}
 
 
+function MDTable(/*DOMElement*/rootElement) {
+	this.rootElement = rootElement;
+	this.rows = [];
+	for ( var i = 0; i < 10; i ++ ) {
+		this.addEmptyRow();
+	}
+}
 
-TableObject.prototype.starttimer = function() {
+MDTable.prototype.getRowCount = function() {
+	return this.rows.length;
+}
+
+MDTable.prototype.addEmptyRow = function() {
+	var index = this.getRowCount();
+	var row = new MDTableRow(index);
+	this.rows[index] = row;
+	this.clearRow(index);
+	this.rootElement.appendChild(row.getRowElement());
+	return row;
+}
+
+MDTable.prototype.getRow = function(/*Index*/index) {
+	if ( index < 0 || index >= this.getRowCount() ) {
+		throw new Error(index, "Row index out of range");
+	}
+	return this.rows[index];
+}
+
+MDTable.prototype.clearRow = function(/*Index*/index) {
+	this.getRow(index).update(new MDEntry(null));
+}
+
+MDTable.prototype.updateRow = function(/*Integer*/index, /*MDEntry*/entry) {
+	this.getRow(index).update(entry);
+}
+
+function MDApp() {
+	this.tableModel = new MDTableModel();
+	this.tableView = new MDTable(document.getElementById("market_body"));
+	this.timerID = null;
+	this.delay = 500;
+	this.pageNumber = 0;
+}
+
+MDApp.prototype.start = function() {
+	this.sendMarketDataRequest();
+}
+
+MDApp.prototype.onTimerTick = function() {
+	this.sendMarketDataRequest();
+}
+
+MDApp.prototype.onMarketDepthData = function(/*Object*/response) {
+	if ( response.error == 1 ) {
+		alert("API error: " + response.error_message);
+	} else {
+		var timestamp = response.timestamp;
+		var data = response.data;		
+		if ( timestamp != null ) {
+			this.tableModel.setTimestamp(timestamp);
+		}
+		for ( var i = 0; i < data.length; i ++ ) {
+			var x = data[i];
+			var entry = new MDEntry(x['sym']);
+			var asks = x['ask'];
+			var bids = x['bid'];
+			for ( var j = 0; j < asks.length; j ++ ) {
+				entry.addAsk(new Quote(asks[j][0], asks[j][1]));
+			}
+			for ( var j = 0; j < bids.length; j ++ ) {
+				entry.addBid(new Quote(bids[j][0], bids[j][1]));
+			}
+			this.tableModel.updateEntry(entry);
+		}
+	}
+	this.refreshTableView();
 	var object = this;
-	this.timeID = setTimeout(function() { object.SendRequest(); }, 1500 );
-	object.refresh();
+	this.timeID = setTimeout(function() { object.onTimerTick(); }, this.delay );
 }
 
-TableObject.prototype.SendRequest = function() {
+MDApp.prototype.onError = function(/*String*/textStatus) {
+	alert("Cannot load table data: " + textStatus);
+}
+
+MDApp.prototype.sendMarketDataRequest = function() {
 	var object = this;
+	var params = {};
+	var timestamp = this.tableModel.getTimestamp();
+	if ( timestamp != null ) {
+		params['timestamp'] = timestamp;
+	}
 	$.ajax({                                      
-      url: 'api.php',                  //the script to call to get data          
-      data: "{2015-12-07 17:27:22.000}",                        //you can insert url argumnets here to pass to api.php
-      type: "GET",                                 //for example "id=5&parent=6"
-      dataType: 'json',                //data format      
-      success: function(data) {
-		var size = data.data.length;
-		var ask2 = " ";
-        var ask1 = " ";
-        var askSize = " ";
-        var askPrice = " ";
-
-        var bid2 = " ";
-        var bid1 = " ";
-        
-        var bidSize = " ";
-        var bidPrice = " ";	
-
-        for(var i =0; i < size; i++) {
-        var sym = data.data[i].sym;
-        var ask2 = data.data[i].ask[0][1];
-        var ask1 = data.data[i].ask[1][1];
-        if(typeof(data.data[i].ask[2]) !== "undefined" )  {
-        var askSize = data.data[i].ask[2][1];
-        var askPrice = data.data[i].ask[2][0];
-        }
-
-        var bid2 = data.data[i].bid[0][1];
-        var bid1 = data.data[i].bid[1][1];
-        if(data.data[i].bid[2] != null)  {
-        var bidSize = data.data[i].bid[2][1];
-        var bidPrice = data.data[i].bid[2][0];
-    }
-        var entry = new DataEntry(sym, bid1, bid2, bidSize, bidPrice, askPrice, askSize, ask1, ask2);
-        var z = 1;
-        updateEntryBySymbol(entry);
-	     }   
-        
-        object.refresh();
-        object.starttimer();
+		url: 'api.php',
+		data: params,
+		type: "GET",
+		dataType: 'json',
+		success: function(response) {
+			object.onMarketDepthData(response)
 		},
-	  error: function(jqXHR, textStatus, errorThrown) {
-			object.onTableDataError(textStatus);
+		error: function(jqXHR, textStatus, errorThrown) {
+			object.onError(textStatus);
 		}
 	});
 }
 
-
-
-TableObject.prototype.onTableDataError = function(/*String*/ textStatus) {
-	alert("Cannot load table data: " + textStatus);
+MDApp.prototype.getTotalPages = function() {
+	var entryCount = this.tableModel.getEntryCount();
+	var rowCount = this.tableView.getRowCount();
+	var pages = Math.floor(entryCount / rowCount);
+	if ( entryCount % rowCount != 0 ) {
+		pages ++;
+	}
 }
 
-
-TableObject.prototype.getTablePage = function() {
-	return this.table_page;
+MDApp.prototype.refreshTableView = function() {
+	var entryCount = this.tableModel.getEntryCount();
+	var rowCount = this.tableView.getRowCount();
+	var startIndex = this.pageNumber * rowCount;
+	for ( var i = 0; i < rowCount; i ++ ) {
+		var entryIndex = startIndex + i;
+		if ( entryIndex >= entryCount ) {
+			this.tableView.clearRow(i);
+		} else {
+			var entry = this.tableModel.getEntryByIndex(entryIndex);
+			this.tableView.updateRow(i, entry);
+		}
+	}
 }
 
-TableObject.prototype.NextTablePage = function() {
-	this.table_page++;
-	if(this.table_page > 10) this.table_page = 10;
-	this.refresh();
+MDApp.prototype.nextPage = function() {
+	this.pageNumber ++;
+	var total = this.getTotalPages();
+	if( this.pageNumber >= total ) {
+		this.pageNumber = total - 1;
+	}
+	this.refreshTableView();
 }
-TableObject.prototype.PrevTablePage = function() {
-	this.table_page--;
-	if(this.table_page < 0) this.table_page = 0;
-	this.refresh();
+
+MDApp.prototype.prevPage = function() {
+	this.pageNumber --;
+	if ( this.pageNumber < 0 ) {
+		this.pageNumber = 0;
+	}
+	this.refreshTableView();
+}
+
+function MDAppLoad() {
+	app = new MDApp();
+	app.start();
 }
 
 function Next() {
-	
-	pager.NextTablePage();
+	app.nextPage();
 
 }
+
 function Previous() {
-	pager.PrevTablePage();
+	app.prevPage();
 }
