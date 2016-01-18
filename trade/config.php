cd6420ad9810d3698a069417734ab6956ff431e5
sergey104 <fil@localhost.localdomain>
2016-01-18 06:28:57 +0300

    new version commit

---
+++ trade/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+define('DEBUG', false, true);
+define('DB_HOST', '127.0.0.1', true);
+define('DB_USER', 'root', true);
+define('DB_PASS', 'Remcolor777', true);
+define('DB_NAME', 'spotquoting', true);
