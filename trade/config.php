3bd408b043c29a739d9458cdd1711d842780a96e
sergey104 <fil@localhost.localdomain>
2016-01-18 12:07:36 +0300

    new version development

--- trade/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-define('DEBUG', false, true);
-define('DB_HOST', '127.0.0.1', true);
-define('DB_USER', 'root', true);
-define('DB_PASS', 'Remcolor777', true);
-define('DB_NAME', 'spotquoting', true);
